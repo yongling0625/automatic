317fd965deb11f7d8c56f0a337f9059be9af1c4f
weijunjiejie <273858601@qq.com>
2016-10-31 12:23:26 +0800

    5

--- src/main/java/com/gy/automatic/controller/HomeController.java
+++ src/main/java/com/gy/automatic/controller/HomeController.java
@@ -38,7 +38,7 @@ public class HomeController {
 		return picMap;
 
 	}
-	public Map<String,Object> selectCarouselPic2227() {
+	public Map<String,Object> selectCarouselPic227() {
 		Map<String, Object> picMap = new HashMap<String, Object>();
 		picMap.put("title", "轮播图片");
 		picMap.put("id", 1);
